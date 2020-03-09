2b2edb8dfaf0da81d7310d86d59bc73e85c9e91f
Qairkhan <shyngys94@gmail.com>
2020-03-09 12:46:13 +0300

    satr my way

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const App=()=> {
+  return (
+    <div className="App">
+      <ul>
+        <li>i chto   </li>
+        <li>s togo   </li>
+        <li>chto    </li>
+      </ul>
+      
+    </div>
+  );
+}
+
+export default App;
