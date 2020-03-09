8b0b817b10a531737ae072086012ea1e84551fea
Qairkhan <shyngys94@gmail.com>
2020-03-09 16:50:35 +0300

    change structure

--- src/App.js
+++ src/App.js
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
-import Header from './components/Header';
-import Navbar from './components/navbar';
-import Profile from './components/profile';
+import Header from './components/Header/Header';
+import Navbar from './components/Navbar/Navbar';
+import Profile from './components/Profile/Profile';
 
 
 const App = () => {
